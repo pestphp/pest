6aa03565704170ed5bef314e8a1f0a635e4f2f49
Adrian Nürnberger <adrian@nuernberger.me>
2020-06-19 23:22:14 +0200

    add more tests

--- tests/Features/Depends.php
+++ tests/Features/Depends.php
@@ -38,3 +38,7 @@ test('depends with defined arguments', function (string $first, string $second) 
 test('depends run test only once', function () use (&$runCounter) {
     assertEquals(2, $runCounter);
 })->depends('first', 'second');
+
+test('incomplete')->skip('incomplete');
+
+test('depends on incomplete')->depends('incomplete')->doesNotPerformAssertions();
