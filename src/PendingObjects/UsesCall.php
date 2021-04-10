d048d60d04b377b242273abdcc1194f00aaa4db9
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-04-10 11:51:13 -0400

    Ignore the absence of the tests folder

--- src/PendingObjects/UsesCall.php
+++ src/PendingObjects/UsesCall.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Pest\PendingObjects;
 
-use Pest\Exceptions\InvalidUsesPath;
 use Pest\TestSuite;
 
 /**
@@ -77,14 +76,13 @@ final class UsesCall
                 ]);
         }, $targets);
 
-        $this->targets = array_map(function ($target): string {
-            $isValid = is_dir($target) || file_exists($target);
-            if (!$isValid) {
-                throw new InvalidUsesPath($target);
+        $this->targets = array_reduce($targets, function (array $accumulator, string $target): array {
+            if (is_dir($target) || file_exists($target)) {
+                $accumulator[] = (string) realpath($target);
             }
 
-            return (string) realpath($target);
-        }, $targets);
+            return $accumulator;
+        }, []);
     }
 
     /**
