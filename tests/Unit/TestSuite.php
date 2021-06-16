e21e3080e0bc4f6ff2ebff50bbbb3796d3edaafd
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2021-06-16 14:04:50 -0500

    test: add new exception check & update snapshots

--- tests/Unit/TestSuite.php
+++ tests/Unit/TestSuite.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pest\Exceptions\DatasetMissing;
 use Pest\Exceptions\TestAlreadyExist;
 use Pest\TestSuite;
 
@@ -9,5 +10,13 @@ it('does not allow to add the same test description twice', function () {
     $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
     $this->expectException(TestAlreadyExist::class);
     $this->expectExceptionMessage(sprintf('A test with the description `%s` already exist in the filename `%s`.', 'foo', __FILE__));
+    $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
+});
+
+it('alerts users about tests with arguments but no input', function () {
+    $testSuite = new TestSuite(getcwd(), 'tests');
+    $test = function (int $arg) {};
+    $this->expectException(DatasetMissing::class);
+    $this->expectExceptionMessage(sprintf("A test with the description '%s' has %d argument(s) ([%s]) and no dataset(s) provided in %s", 'foo', 1, 'int $arg', __FILE__));
     $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
 });
