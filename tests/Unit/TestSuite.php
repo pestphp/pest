e64856c664f9cb530a4c70eb779187ed125e3710
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2021-06-17 13:33:11 -0500

    test: use throws method instead of assert

--- tests/Unit/TestSuite.php
+++ tests/Unit/TestSuite.php
@@ -8,15 +8,17 @@ it('does not allow to add the same test description twice', function () {
     $testSuite = new TestSuite(getcwd(), 'tests');
     $test = function () {};
     $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
-    $this->expectException(TestAlreadyExist::class);
-    $this->expectExceptionMessage(sprintf('A test with the description `%s` already exist in the filename `%s`.', 'foo', __FILE__));
     $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
-});
+})->throws(
+    TestAlreadyExist::class,
+    sprintf('A test with the description `%s` already exist in the filename `%s`.', 'foo', __FILE__),
+);
 
 it('alerts users about tests with arguments but no input', function () {
     $testSuite = new TestSuite(getcwd(), 'tests');
     $test = function (int $arg) {};
-    $this->expectException(DatasetMissing::class);
-    $this->expectExceptionMessage(sprintf("A test with the description '%s' has %d argument(s) ([%s]) and no dataset(s) provided in %s", 'foo', 1, 'int $arg', __FILE__));
     $testSuite->tests->set(new \Pest\Factories\TestCaseFactory(__FILE__, 'foo', $test));
-});
+})->throws(
+    DatasetMissing::class,
+    sprintf("A test with the description '%s' has %d argument(s) ([%s]) and no dataset(s) provided in %s", 'foo', 1, 'int $arg', __FILE__),
+);
