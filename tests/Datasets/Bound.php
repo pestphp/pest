d8fae6d689db529ce66e916333253391d0446b39
luke <lukeraymonddowning@gmail.com>
2021-07-28 10:39:39 +0100

    Datasets can now access the test case and are executed after the setup method has run.

---
+++ tests/Datasets/Bound.php
@@ -0,0 +1,11 @@
+<?php
+
+dataset('bound.closure', function () {
+    yield function () { return 1; };
+    yield function () { return 2; };
+});
+
+dataset('bound.array', [
+    function () { return 1; },
+    function () { return 2; },
+]);
