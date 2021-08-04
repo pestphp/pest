9ec0762d4114b2aaaaf3cd3470b59f623835b87c
Nuno Maduro <enunomaduro@gmail.com>
2021-08-04 22:56:18 +0100

    tests: refactors `toBeTruthy` and `toBeFalsy`

---
+++ tests/Features/Expect/toBeFalsy.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\ExpectationFailedException;
+
+test('passes as falsy', function ($value) {
+    expect($value)->toBeFalsy();
+})->with([false, '', null, 0, '0']);
+
+test('passes as not falsy', function ($value) {
+    expect($value)->not->toBeFalsy();
+})->with([true, [1], 'false', 1, -1]);
+
+test('failures', function () {
+    expect(1)->toBeFalsy();
+})->throws(ExpectationFailedException::class);
+
+test('not failures', function () {
+    expect(null)->not->toBeFalsy();
+})->throws(ExpectationFailedException::class);
---
+++ tests/Features/Expect/toBeTruthy.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\ExpectationFailedException;
+
+test('passes as truthy', function ($value) {
+    expect($value)->toBeTruthy();
+})->with([true, [1], 'false', 1, -1]);
+
+test('passes as not truthy', function ($value) {
+    expect($value)->not->toBeTruthy();
+})->with([false, '', null, 0, '0']);
+
+test('failures', function () {
+    expect(null)->toBeTruthy();
+})->throws(ExpectationFailedException::class);
+
+test('not failures', function () {
+    expect(1)->not->toBeTruthy();
+})->throws(ExpectationFailedException::class);
