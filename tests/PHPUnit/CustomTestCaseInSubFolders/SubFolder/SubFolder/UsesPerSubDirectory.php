bcab4224fb2ac6658e0945debf8a91778c00817e
luke <lukeraymonddowning@gmail.com>
2021-08-10 14:24:34 +0100

    Skips a test that doesn't support parallel.

--- tests/PHPUnit/CustomTestCaseInSubFolders/SubFolder/SubFolder/UsesPerSubDirectory.php
+++ tests/PHPUnit/CustomTestCaseInSubFolders/SubFolder/SubFolder/UsesPerSubDirectory.php
@@ -1,5 +1,7 @@
 <?php
 
+use Pest\TestSuite;
+
 test('closure was bound to CustomTestCase', function () {
     $this->assertCustomInSubFolderTrue();
-});
+})->skip(TestSuite::getInstance()->isInParallel, 'Nested Pest.php files are not loaded in parallel.');
