fecd7066dad54d217c7ff48b723f4f60478ee23e
Luke Downing <lukeraymonddowning@gmail.com>
2022-01-06 16:28:08 +0000

    CS

--- tests/Features/Datasets.php
+++ tests/Features/Datasets.php
@@ -288,8 +288,10 @@ it('can correctly resolve a bound dataset that returns an array but wants to be 
 it('can specify a single item in the dataset to run', function (Closure $closure) {
     $closure();
 })->throws('This should run')->with([
-    'this should not run' => fn () => throw new Exception('This should have been skipped'),
-    '>>>this should run' => fn () => throw new Exception('This should run'),
-    'this also should not run' => fn () => throw new Exception('This should have been skipped'),
+    'this should not run'         => fn ()         => throw new Exception('This should have been skipped'),
+    '>>>this should run'          => fn ()          => throw new Exception('This should run'),
+    'this also should not run'    => fn ()    => throw new Exception('This should have been skipped'),
     '>>> but this one should run' => fn () => throw new Exception('This should run'),
+    fn ()                         => throw new Exception('This should have been skipped'),
+    '>>>'                         => fn ()                         => throw new Exception('This should run'),
 ]);
