984f237a928042937b6a9575e964b0e575753d35
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-03 15:45:41 +0100

    phpstan

--- src/Logging/JUnit.php
+++ src/Logging/JUnit.php
@@ -173,7 +173,6 @@ final class JUnit extends Printer implements TestListener
         $this->doAddSkipped();
     }
 
-    /** @phpstan-ignore-next-line */
     public function startTestSuite(TestSuite $suite): void
     {
         $testSuite = $this->document->createElement('testsuite');
@@ -212,7 +211,6 @@ final class JUnit extends Printer implements TestListener
         $this->testSuiteTimes[$this->testSuiteLevel]      = 0;
     }
 
-    /** @phpstan-ignore-next-line */
     public function endTestSuite(TestSuite $suite): void
     {
         $this->testSuites[$this->testSuiteLevel]->setAttribute(
