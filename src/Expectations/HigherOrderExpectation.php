3e325e336455402f2596cb9c5f9e5786c8ba7f6c
Nuno Maduro <enunomaduro@gmail.com>
2022-11-09 20:34:18 +0000

    chore: improves type checking

--- src/Expectations/HigherOrderExpectation.php
+++ src/Expectations/HigherOrderExpectation.php
@@ -161,7 +161,6 @@ final class HigherOrderExpectation
      */
     private function getValue(): mixed
     {
-        // @phpstan-ignore-next-line
         return $this->shouldReset ? $this->original->value : $this->expectation->value;
     }
 
