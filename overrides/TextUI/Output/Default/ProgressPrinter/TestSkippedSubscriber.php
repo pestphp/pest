8ee7a4deef334f359007efdb8900fc2d4b92a98a
Luke Downing <lukeraymonddowning@gmail.com>
2023-02-13 11:19:18 +0000

    chore(cleanup): Tidy-up and tweaks of Pest Parallel integration.

--- overrides/TextUI/Output/Default/ProgressPrinter/TestSkippedSubscriber.php
+++ overrides/TextUI/Output/Default/ProgressPrinter/TestSkippedSubscriber.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 /*
  * This file is part of PHPUnit.
  *
@@ -7,6 +9,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace PHPUnit\TextUI\Output\Default\ProgressPrinter;
 
 use PHPUnit\Event\Test\Skipped;
