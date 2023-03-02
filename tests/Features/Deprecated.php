3df68b288ad98e7c12129c4b455b8ff517d2d2b4
Nuno Maduro <enunomaduro@gmail.com>
2023-03-02 17:51:21 +0000

    tests: deprecations

---
+++ tests/Features/Deprecated.php
@@ -0,0 +1,13 @@
+<?php
+
+test('deprecated', function () {
+    str_contains(null, null);
+
+    expect(true)->toBeTrue();
+});
+
+test('user deprecated', function () {
+    trigger_deprecation('foo', '1.0', 'This is a deprecation description');
+
+    expect(true)->toBeTrue();
+});
