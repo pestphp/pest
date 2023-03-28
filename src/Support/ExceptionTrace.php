e109cd1da26f2661c19785a94e8b27c35b27137c
Nuno Maduro <enunomaduro@gmail.com>
2023-03-28 01:48:46 +0100

    feat: `uses()`hint

--- src/Support/ExceptionTrace.php
+++ src/Support/ExceptionTrace.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Pest\Support;
 
 use Closure;
+use PHPUnit\Framework\TestCase;
 use Throwable;
 
 /**
@@ -15,8 +16,7 @@ final class ExceptionTrace
     private const UNDEFINED_METHOD = 'Call to undefined method P\\';
 
     /**
-     * Ensures the given closure reports
-     * the good execution context.
+     * Ensures the given closure reports the good execution context.
      *
      * @return mixed
      *
@@ -28,7 +28,13 @@ final class ExceptionTrace
             return $closure();
         } catch (Throwable $throwable) {
             if (Str::startsWith($message = $throwable->getMessage(), self::UNDEFINED_METHOD)) {
+                $class = preg_match('/^Call to undefined method ([^:]+)::/', $message, $matches) === false ? null : $matches[1];
+
                 $message = str_replace(self::UNDEFINED_METHOD, 'Call to undefined method ', $message);
+
+                if (class_exists($class) && count(class_parents($class)) > 0 && array_values(class_parents($class))[0] === TestCase::class) {
+                    $message .= '. Did you forget to use the [uses()] function? https://pestphp.com/docs/configuring-tests';
+                }
 
                 Reflection::setPropertyValue($throwable, 'message', $message);
             }
---
+++ tests/Unit/Support/ExceptionTrace.php
@@ -0,0 +1,21 @@
+<?php
+
+use Pest\Support\ExceptionTrace;
+
+it('ensures the given closures reports the correct class name', function () {
+    $closure = function () {
+        throw new Exception('Call to undefined method P\Tests\IntentionallyNotExisting::testBasic().');
+    };
+
+    ExceptionTrace::ensure($closure);
+})->throws(
+    Exception::class,
+    'Call to undefined method Tests\IntentionallyNotExisting::testBasic().',
+);
+
+it('ensures the given closures reports the correct class name and suggests the [uses()] function', function () {
+    $this->get();
+})->throws(
+    Error::class,
+    'Call to undefined method Tests\Unit\Support\ExceptionTrace::get(). Did you forget to use the [uses()] function? https://pestphp.com/docs/configuring-tests',
+);
