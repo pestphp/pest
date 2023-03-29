1965763cd07563d30ada751ca3e0e8fdfca67608
Nikos Charalampidis <n.charalampidis@netstudio.gr>
2023-03-29 13:02:18 +0300

    fix parallel execution of test class that inherit other class with the same name

---
+++ tests/Fixtures/Inheritance/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Fixtures\Inheritance;
+
+class ExampleTest extends Base\ExampleTest
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
