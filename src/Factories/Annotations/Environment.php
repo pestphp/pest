0c33379d56a174ca398d9ff1636a3a094b5a3898
Tim Smith <tim.smith@titan21.co.uk>
2023-05-10 12:16:05 +0100

    Fixing static analysis issues

--- src/Factories/Annotations/Environment.php
+++ src/Factories/Annotations/Environment.php
@@ -8,11 +8,17 @@ use Pest\Contracts\AddsAnnotations;
 use Pest\Factories\Testbench\Environment as EnvironmentFactory;
 use Pest\Factories\TestCaseMethodFactory;
 
+/**
+ * @internal
+ */
 final class Environment implements AddsAnnotations
 {
+    /**
+     * {@inheritdoc}
+     */
     public function __invoke(TestCaseMethodFactory $method, array $annotations): array
     {
-        if(($method->environment[0] ?? null) instanceof EnvironmentFactory) {
+        if (($method->environment[0] ?? null) instanceof EnvironmentFactory) {
             $annotations[] = "@define-env {$method->environment[0]->name}";
         }
 
--- tests/Features/Testbench.php
+++ tests/Features/Testbench.php
@@ -4,7 +4,7 @@ function my_environment($app)
 {
 }
 
-test('it will add a define-env annotation', function() {
+test('it will add a define-env annotation', function () {
     $phpDoc = (new ReflectionClass($this))->getMethod($this->name());
     expect(str_contains($phpDoc->getDocComment(), '* @define-env my_environment'))->toBeTrue();
 })->environment('my_environment');
