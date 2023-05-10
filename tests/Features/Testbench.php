ac22d2d2301419fa74aa8dddddd731edacd84a98
Tim Smith <tim.smith@titan21.co.uk>
2023-05-10 13:00:18 +0100

    Simplifying test

--- tests/Features/Testbench.php
+++ tests/Features/Testbench.php
@@ -6,5 +6,5 @@ function my_environment($app)
 
 test('it will add a define-env annotation', function () {
     $phpDoc = (new ReflectionClass($this))->getMethod($this->name());
-    expect(str_contains($phpDoc->getDocComment(), '* @define-env my_environment'))->toBeTrue();
+    expect($phpDoc->getDocComment())->toContain('* @define-env my_environment');
 })->environment('my_environment');
