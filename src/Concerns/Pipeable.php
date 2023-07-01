b5f89d1ff88c42a78d7501f3aef2409e211a56a7
Nuno Maduro <enunomaduro@gmail.com>
2023-07-01 10:28:15 +0100

    chore: bumps dependencies

--- src/Concerns/Pipeable.php
+++ src/Concerns/Pipeable.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pest\Concerns;
 
 use Closure;
-use Pest\Expectation;
 
 /**
  * @internal
--- src/Support/ExceptionTrace.php
+++ src/Support/ExceptionTrace.php
@@ -32,7 +32,7 @@ final class ExceptionTrace
 
                 $message = str_replace(self::UNDEFINED_METHOD, 'Call to undefined method ', $message);
 
-                if (class_exists($class) && count(class_parents($class)) > 0 && array_values(class_parents($class))[0] === TestCase::class) {
+                if (class_exists((string) $class) && (is_countable(class_parents($class)) ? count(class_parents($class)) : 0) > 0 && array_values(class_parents($class))[0] === TestCase::class) { // @phpstan-ignore-line
                     $message .= '. Did you forget to use the [uses()] function? Read more at: https://pestphp.com/docs/configuring-tests';
                 }
 
