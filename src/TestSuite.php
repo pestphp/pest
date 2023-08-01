0518971d2f371041ff4fa80c0ac67a09adff1bd4
Fabio Ivona <fabio.ivona@defstudio.it>
2023-08-01 17:16:50 +0200

    refactor

--- src/TestSuite.php
+++ src/TestSuite.php
@@ -128,6 +128,6 @@ final class TestSuite
     {
         assert($this->test instanceof TestCase);
 
-        (fn () => $this->__snapshotChanges[] = $message)->call($this->test, $this->test::class); // @phpstan-ignore-line
+        (fn (): string => $this->__snapshotChanges[] = $message)->call($this->test, $this->test::class); // @phpstan-ignore-line
     }
 }
