2b3464e708d812b924dd9ea3cd8eae17cc9948ea
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2023-08-13 22:59:46 +0100

    add dataset for failures

--- tests/Features/Expect/toBePluralOf.php
+++ tests/Features/Expect/toBePluralOf.php
@@ -2,7 +2,7 @@
 
 use PHPUnit\Framework\ExpectationFailedException;
 
-it('passes', function (string $pluralizedWord, string $singularWord) {
+test('passes', function (string $pluralizedWord, string $singularWord) {
     expect($pluralizedWord)->toBePluralOf($singularWord);
 })->with([
     ['schools', 'school'],
@@ -11,7 +11,7 @@ it('passes', function (string $pluralizedWord, string $singularWord) {
     ['aliases', 'alias'],
     ['buses', 'bus'],
     ['loaves', 'loaf'],
-    ['parentheses', 'parenthesis'],
+    ['potatoes', 'potato'],
     ['children', 'child'],
     ['people', 'person'],
     ['teeth', 'tooth'],
@@ -29,9 +29,27 @@ it('passes', function (string $pluralizedWord, string $singularWord) {
     ['commands', 'command'],
 ]);
 
-test('failures', function () {
-    expect('dog')->toBePluralOf('dogs');
-})->throws(ExpectationFailedException::class);
+test('failures', function (string $pluralizedWord, string $singularWord) {
+    expect($pluralizedWord)->toBePluralOf($singularWord);
+})->with([
+    ['school', 'school'],
+    ['quiz', 'quiz'],
+    ['tomato', 'tomato'],
+    ['alias', 'alias'],
+    ['bus', 'bus'],
+    ['loaf', 'loaf'],
+    ['potato', 'potato'],
+    ['child', 'child'],
+    ['person', 'person'],
+    ['tooth', 'tooth'],
+    ['man', 'man'],
+    ['nucleus', 'nucleus'],
+    ['vertex', 'vertex'],
+    ['dog', 'dog'],
+    ['relation', 'relation'],
+    ['shine', 'shine'],
+    ['command', 'command'],
+])->throws(ExpectationFailedException::class);
 
 test('failures with custom message', function () {
     expect('dog')->toBePluralOf('dogs', 'oh no!');
