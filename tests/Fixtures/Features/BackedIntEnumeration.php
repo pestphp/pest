e8356ae9b184c9c54ab420cecb275003b3131cae
AJ Meireles <alvaro.meireles@live.com>
2023-08-23 20:34:23 -0300

    separating fixtures

---
+++ tests/Fixtures/Features/BackedIntEnumeration.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Fixtures\Features;
+enum BackedIntEnumeration: int
+{
+    case Foo = 1;
+    case Bar = 2;
+}
--- tests/Fixtures/Features/BackedStringEnumeration.php
+++ tests/Fixtures/Features/BackedStringEnumeration.php
@@ -7,9 +7,3 @@ enum BackedStringEnumeration: string
     case Foo = 'foo';
     case Bar = 'bar';
 }
-
-enum BackedIntEnumeration: int
-{
-    case Foo = 1;
-    case Bar = 2;
-}
