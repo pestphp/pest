9fb64599de3a336ef8c59754c0a95d6eca1c15bb
Sarfaraz Muhammad Sajib <50302555+Muhammad-Sarfaraz@users.noreply.github.com>
2023-10-16 10:56:06 +0600

    Polishing Up "TestDox.php' PHPDoc Blocks for Clarity
    Added the missing parenthesis and period for proper punctuation and formatted the doc block to meet PHPDocumentor standards.

--- src/Factories/Annotations/TestDox.php
+++ src/Factories/Annotations/TestDox.php
@@ -14,13 +14,12 @@ final class TestDox implements AddsAnnotations
      */
     public function __invoke(TestCaseMethodFactory $method, array $annotations): array
     {
-        /*
-       * escapes docblock according to
+      /*
+       * Escapes docblock according to
        * https://manual.phpdoc.org/HTMLframesConverter/default/phpDocumentor/tutorial_phpDocumentor.howto.pkg.html#basics.desc
        *
-       * note: '@' escaping is not needed as it cannot be the first character of the line (it always starts with @testdox
+       * note: '@' escaping is not needed as it cannot be the first character of the line (it always starts with @testdox).
        */
-
         assert($method->description !== null);
         $methodDescription = str_replace('*/', '{@*}', $method->description);
 
