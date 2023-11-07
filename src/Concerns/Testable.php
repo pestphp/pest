95ec0a82b2a571647528ac07dd398d4f3612acd6
faissaloux <fwahabali@gmail.com>
2023-11-07 17:35:42 +0100

    fix html in tests descriptions and datasets

--- src/Concerns/Testable.php
+++ src/Concerns/Testable.php
@@ -193,6 +193,7 @@ trait Testable
         $method = TestSuite::getInstance()->tests->get(self::$__filename)->getMethod($this->name());
 
         $description = $this->dataName() ? $method->description.' with '.$this->dataName() : $method->description;
+        $description = htmlspecialchars(html_entity_decode($description), ENT_NOQUOTES);
 
         if ($method->repetitions > 1) {
             $matches = [];
