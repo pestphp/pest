c4ab76253012df96b1bce52e8b913d4b295e5835
faissaloux <fwahabali@gmail.com>
2023-11-09 02:03:40 +0100

    add more toContainOp tests

--- tests/Features/Expect/toContainOp.php
+++ tests/Features/Expect/toContainOp.php
@@ -7,12 +7,36 @@ test('passes strings', function () {
     expect('Nuno')->toContainOp('greater than or equal', 1, 'Nu');
 });
 
+test('not success with greater than', function () {
+    expect([1, 42, 2, 42, 31, 42])->not->toContainOp('greater than', 2, 31);
+});
+
+test('passes with greater than', function () {
+    expect([[1, 2, 3], 2, 42, [1, 2, 3]])->toContainOp('greater than', 1, [1, 2, 3]);
+});
+
 test('not success with greater than or equal', function () {
     expect([1, 42, 2, 42, 31, 42])->not->toContainOp('greater than or equal', 2, 31);
 });
 
 test('passes with greater than or equal', function () {
     expect([[1, 2, 3], 2, 42, [1, 2, 3]])->toContainOp('greater than or equal', 1, [1, 2, 3]);
+});
+
+test('not success with less than', function () {
+    expect([1, 42, 2, 42, 31, 42])->not->toContainOp('less than', 2, 42);
+});
+
+test('passes with less than', function () {
+    expect([[1, 2, 3], 2, 42, [1, 2, 3]])->toContainOp('less than', 3, [1, 2, 3]);
+});
+
+test('not success with less than or equal', function () {
+    expect([1, 42, 2, 42, 31, 42])->not->toContainOp('less than or equal', 2, 42);
+});
+
+test('passes with less than or equal', function () {
+    expect([[1, 2, 3], 2, 42, [1, 2, 3]])->toContainOp('less than or equal', 2, [1, 2, 3]);
 });
 
 test('not success with equals', function () {
