da20a62e49180c579c1006ed56005dffe17c6b6e
Mark Huot <mark@markhuot.com>
2023-11-21 22:56:21 -0500

    Add toSnapshot() early return
    Sometimes objects need native toString() and toArray() methods that are different from what you want to snapshot.
    
    This adds an explicit toSnapshot() method that will be called first (when set) allowing for better snapshot values than the generic methods offer.

--- src/Mixins/Expectation.php
+++ src/Mixins/Expectation.php
@@ -844,6 +844,7 @@ final class Expectation
 
         $string = match (true) {
             is_string($this->value) => $this->value,
+            is_object($this->value) && method_exists($this->value, 'toSnapshot') => $this->value->toSnapshot(),
             is_object($this->value) && method_exists($this->value, '__toString') => $this->value->__toString(),
             is_object($this->value) && method_exists($this->value, 'toString') => $this->value->toString(),
             $this->value instanceof \Illuminate\Testing\TestResponse => $this->value->getContent(), // @phpstan-ignore-line
