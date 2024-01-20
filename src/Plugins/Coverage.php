108d181a0502b5888a42fe3537990934a8f76c0c
Davy Baccaert <davy.baccaert@steelseries.gg>
2024-01-20 15:29:35 +0100

    Improve coverage output message on failing minimum requirements

--- src/Plugins/Coverage.php
+++ src/Plugins/Coverage.php
@@ -128,9 +128,9 @@ final class Coverage implements AddsOutput, HandlesArguments
 
             if ($exitCode === 1) {
                 $this->output->writeln(sprintf(
-                    "\n  <fg=white;bg=red;options=bold> FAIL </> Code coverage below expected:<fg=red;options=bold> %s %%</>. Minimum:<fg=white;options=bold> %s %%</>.",
-                    number_format($coverage, 1),
-                    number_format($this->coverageMin, 1)
+                    "\n  <fg=white;bg=red;options=bold> FAIL </> Code coverage below expected <fg=white;options=bold> %s %%</>, currently <fg=red;options=bold> %s %%</>.",
+                    number_format($this->coverageMin, 1),
+                    number_format($coverage, 1)
                 ));
             }
 
