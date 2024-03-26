0151b5a6878d7cb73078d55398e2f3cfb4d74c29
Fabio Ivona <fabio.ivona@defstudio.it>
2024-03-26 11:57:25 +0100

    test

--- tests/Features/ScopedDatasets/TestFileOutOfScope.php
+++ tests/Features/ScopedDatasets/TestFileOutOfScope.php
@@ -11,17 +11,25 @@ test('the right dataset is taken', function () use ($state) {
     expect($state->text)->toBe('12');
 });
 
-// it('can see datasets defined in Pest.php file', function (string $value) use ($state) {
-//     $state->text .= $value;
-//     expect(true)->toBe(true);
-// })->with('dataset_in_pest_file');
-//
-// test('Pest.php dataset is taken', function () use ($state) {
-//     expect($state->text)->toBe('12AB');
-// });
+it('can see datasets defined in Pest.php file', function (string $value) use ($state) {
+    $state->text .= $value;
+    expect(true)->toBe(true);
+})->with('dataset_in_pest_file');
 
+test('Pest.php dataset is taken', function () use ($state) {
+    expect($state->text)->toBe('12AB');
+});
 
-test('uses datasets in nested directories', function($value) use ($state){
+it('can see datasets defined in Pest.php file 2', function (string $value) use ($state) {
+    $state->text .= $value;
+    expect(true)->toBe(true);
+})->with('dataset_in_pest_file');
+
+test('Pest.php dataset is taken', function () use ($state) {
+    expect($state->text)->toBe('12AB');
+});
+
+test('uses datasets in nested directories', function ($value) use ($state) {
     $state->text .= $value;
     expect(true)->toBe(true);
 })->with('nested.letters');
