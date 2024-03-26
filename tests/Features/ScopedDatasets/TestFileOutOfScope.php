7bae226a15f382dd7b30a6274d071c134e22a228
Fabio Ivona <fabio.ivona@defstudio.it>
2024-03-26 11:55:01 +0100

    test

--- tests/Features/ScopedDatasets/TestFileOutOfScope.php
+++ tests/Features/ScopedDatasets/TestFileOutOfScope.php
@@ -11,14 +11,14 @@ test('the right dataset is taken', function () use ($state) {
     expect($state->text)->toBe('12');
 });
 
-it('can see datasets defined in Pest.php file', function (string $value) use ($state) {
-    $state->text .= $value;
-    expect(true)->toBe(true);
-})->with('dataset_in_pest_file');
-
-test('Pest.php dataset is taken', function () use ($state) {
-    expect($state->text)->toBe('12AB');
-});
+// it('can see datasets defined in Pest.php file', function (string $value) use ($state) {
+//     $state->text .= $value;
+//     expect(true)->toBe(true);
+// })->with('dataset_in_pest_file');
+//
+// test('Pest.php dataset is taken', function () use ($state) {
+//     expect($state->text)->toBe('12AB');
+// });
 
 
 $nestedDatasetState = new stdClass();
