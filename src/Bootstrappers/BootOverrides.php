c7bcb6eb7bb4c6d08d2a00b1f1989a2da1c71fc1
Nuno Maduro <enunomaduro@gmail.com>
2024-06-08 14:01:41 +0100

    chore: updates snapshots

--- src/Bootstrappers/BootOverrides.php
+++ src/Bootstrappers/BootOverrides.php
@@ -19,9 +19,9 @@ final class BootOverrides implements Bootstrapper
      */
     public const FILES = [
         '4f57b79c6ca77cab241cef879ea98bc743d2cd1fbe4586ab652608bf29aa4176' => 'Runner/Filter/NameFilterIterator.php',
-        'c7c09ab7c9378710b27f761a4b2948196cbbdf2a73e4389bcdca1e7c94fa9c21' => 'Runner/ResultCache/DefaultResultCache.php',
-        '6c23c8455e135328ca89b54ac2dfe9ba3d6ce23f9e080928752dbee2de93f0eb' => 'Runner/TestSuiteLoader.php',
-        '2ef8e21dbb27cf6597dd9bb0f941c063dcc98b5af2c35d10b1c2d77721582e8f' => 'TextUI/Command/Commands/WarmCodeCoverageCacheCommand.php',
+        '288a312ae73fa1cea0325e5862f76e5641f1f3f132cf6e3d5df4811c57571c41' => 'Runner/ResultCache/DefaultResultCache.php',
+        'a3daa830857b9fb8fe78606dc07d93dcedb30cf0bddf1829812143541b1ad39f' => 'Runner/TestSuiteLoader.php',
+        '9e8806c684a23d14a9ed2d6fb3102c7be9a438e7e2b96ecfe3b634817e285104' => 'TextUI/Command/Commands/WarmCodeCoverageCacheCommand.php',
         'badc88c79c2a47d768be3925051999b158d08b64e57ccf4ce560f1610cbcc1e8' => 'TextUI/Output/Default/ProgressPrinter/Subscriber/TestSkippedSubscriber.php',
         '5ff38e143e244c4d80e767447e5a045891cc6518f008f24f2bb945289b83a07f' => 'TextUI/TestSuiteFilterProcessor.php',
         'a01a02eadd18146f12731c7adb8cd56cf76f3f6bda2bae06ff4fd6573789b0f4' => 'Event/Value/ThrowableBuilder.php',
