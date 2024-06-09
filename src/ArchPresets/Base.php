cc41a7f81d812e10b8ff755b0cd05ddd18a5f906
Punyapal Shah <mrpunyapal@gmail.com>
2024-06-09 19:16:29 +0530

    feat: add 'eval' to list of expected functions in Base.php

--- src/ArchPresets/Base.php
+++ src/ArchPresets/Base.php
@@ -21,7 +21,7 @@ final class Base implements ArchPreset
     public function boot(TestCall $testCall, array $baseNamespace): TestCall|ArchExpectation
     {
         return $testCall // @phpstan-ignore-line
-            ->expect(['dd', 'dump', 'ray', 'die', 'var_dump', 'sleep'])
+            ->expect(['dd', 'dump', 'ray', 'die', 'var_dump', 'sleep', 'eval'])
             ->not
             ->toBeUsed();
     }
