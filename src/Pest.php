e8f122bf47585c06431e0056189ec6bfd6f41f57
Nuno Maduro <enunomaduro@gmail.com>
2024-06-10 23:02:16 +0100

    release: 2.34.8

--- src/Pest.php
+++ src/Pest.php
@@ -6,7 +6,7 @@ namespace Pest;
 
 function version(): string
 {
-    return '2.34.7';
+    return '2.34.8';
 }
 
 function testDirectory(string $file = ''): string
--- tests/Visual/Parallel.php
+++ tests/Visual/Parallel.php
@@ -16,7 +16,7 @@ $run = function () {
 
 test('parallel', function () use ($run) {
     expect($run('--exclude-group=integration'))
-        ->toContain('Tests:    1 deprecated, 4 warnings, 5 incomplete, 2 notices, 13 todos, 16 skipped, 994 passed (2348 assertions)')
+        ->toContain('Tests:    1 deprecated, 4 warnings, 5 incomplete, 2 notices, 13 todos, 16 skipped, 998 passed (2358 assertions)')
         ->toContain('Parallel: 3 processes');
 })->skipOnWindows();
 
