bb8677549aa6ee9c1994c4ed5e36c451af88c740
Nuno Maduro <enunomaduro@gmail.com>
2024-09-02 18:30:12 +0100

    chore: fixes snapshots

--- tests/Visual/Todo.php
+++ tests/Visual/Todo.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Process\Process;
 
 $run = function (string $target, bool $parallel) {
-    $process = new Process(['php', 'bin/pest', $target, $parallel ? '--parallel' : '', '--colors=always'], dirname(__DIR__, 2),
+    $process = new Process(['php', 'bin/pest', $target, $parallel ? '--parallel' : ''], dirname(__DIR__, 2),
         ['COLLISION_PRINTER' => 'DefaultPrinter', 'COLLISION_IGNORE_DURATION' => 'true'],
     );
 
@@ -18,16 +18,16 @@ $run = function (string $target, bool $parallel) {
 
 test('todos', function () use ($run) {
     expect($run('--todos', false))->toMatchSnapshot();
-})->skipOnWindows();
+})->skipOnWindows()->skip(! getenv('REBUILD_SNAPSHOTS') && getenv('EXCLUDE'));
 
 test('todos in parallel', function () use ($run) {
     expect($run('--todos', true))->toMatchSnapshot();
-})->skipOnWindows();
+})->skipOnWindows()->skip(! getenv('REBUILD_SNAPSHOTS') && getenv('EXCLUDE'));
 
 test('todo', function () use ($run) {
     expect($run('--todo', false))->toMatchSnapshot();
-})->skipOnWindows();
+})->skipOnWindows()->skip(! getenv('REBUILD_SNAPSHOTS') && getenv('EXCLUDE'));
 
 test('todo in parallel', function () use ($run) {
     expect($run('--todo', true))->toMatchSnapshot();
-})->skipOnWindows();
+})->skipOnWindows()->skip(! getenv('REBUILD_SNAPSHOTS') && getenv('EXCLUDE'));
