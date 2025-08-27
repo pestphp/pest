130e554ad6b207fa536bec8c2e3130a49cf1aa4e
flap152 <flap_152@yahoo.ca>
2025-08-26 21:46:14 -0400

    Fixed failing test when adding an empty string element ('') in the arguments array.

--- tests/Visual/Todo.php
+++ tests/Visual/Todo.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Process\Process;
 
 $run = function (string $target, bool $parallel) {
-    $process = new Process(['php', 'bin/pest', $target, $parallel ? '--parallel' : ''], dirname(__DIR__, 2),
+    $process = new Process(['php', 'bin/pest', $target, ...($parallel ? ['--parallel'] : [])], dirname(__DIR__, 2),
         ['COLLISION_PRINTER' => 'DefaultPrinter', 'COLLISION_IGNORE_DURATION' => 'true'],
     );
 
