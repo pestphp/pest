9c232afd2bd2c52a7dbf8661134ac15c56911722
flap152 <flap_152@yahoo.ca>
2025-12-22 10:05:35 -0500

    fix whitespace after manual merge conflict resolution

--- tests/Visual/Parallel.php
+++ tests/Visual/Parallel.php
@@ -17,7 +17,7 @@ $run = function () {
 test('parallel', function () use ($run) {
     expect($run('--exclude-group=integration'))
         ->toContain('Tests:    2 deprecated, 4 warnings, 5 incomplete, 2 notices, 39 todos, 26 skipped, 1179 passed (2792 assertions)')
-         ->toContain('Parallel: 3 processes');
+        ->toContain('Parallel: 3 processes');
 })->skipOnWindows();
 
 test('parallel can have multiple exclude-groups', function () use ($run) {
