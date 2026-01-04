bc57a84e77afd4544ff9643a6858f68d05aeab96
Nuno Maduro <enunomaduro@gmail.com>
2026-01-04 11:29:59 -0500

    release: 4.3.1

--- src/ArchPresets/Php.php
+++ src/ArchPresets/Php.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Pest\ArchPresets;
 
-use Pest\Arch\Contracts\ArchExpectation;
-use Pest\Expectation;
-
 /**
  * @internal
  */
--- src/Pest.php
+++ src/Pest.php
@@ -6,7 +6,7 @@ namespace Pest;
 
 function version(): string
 {
-    return '4.3.0';
+    return '4.3.1';
 }
 
 function testDirectory(string $file = ''): string
