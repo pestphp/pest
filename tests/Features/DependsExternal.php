6a7446051755006f14819f0a95f33464241e9523
Sami Aji <sami@ajility.dev>
2026-04-16 11:53:19 -0400

    feat(depends-external): add test variations

--- tests/Features/DependsExternal.php
+++ tests/Features/DependsExternal.php
@@ -1,6 +1,20 @@
 <?php
 
-test('depends on test in an an external file', function (string $first, string $second) {
+test('dependsExternal', function () {
+    expect(func_get_args())->toBe(['first', 'second']);
+})->dependsExternal('Tests\Features\Depends', 'first', 'second');
+
+test('depends on tests in an external file', function (string $first, string $second) {
+    expect($first)->toBe('first');
+    expect($second)->toBe('second');
+})->dependsExternal('Tests\Features\Depends', 'first', 'second');
+
+test('depends on tests in an external file with spread arguments', function (string ...$params) {
+    expect(func_get_args())->toBe($params);
+    expect($params)->toBe(['first', 'second']);
+})->dependsExternal('Tests\Features\Depends', 'first', 'second');
+
+test('depends on tests in an external file with defined arguments', function (string $first, string $second) {
     expect($first)->toBe('first');
     expect($second)->toBe('second');
 })->dependsExternal('Tests\Features\Depends', 'first', 'second');
