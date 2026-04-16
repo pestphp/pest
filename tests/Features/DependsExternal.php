d25dffc4ebd704f24196db4c9ace7357f5d50584
Sami Aji <sami@ajility.dev>
2026-04-16 14:46:58 -0400

    feat(depends-external): add tests for chaining

--- tests/Features/DependsExternal.php
+++ tests/Features/DependsExternal.php
@@ -4,6 +4,16 @@ test('dependsExternal', function () {
     expect(func_get_args())->toBe(['first', 'second']);
 })->dependsExternal('Tests\Features\Depends', 'first', 'second');
 
+test('dependsExternal chained', function () {
+    expect(func_get_args())->toBe(['first', 'second']);
+})->dependsExternal('Tests\Features\Depends', 'first')
+    ->dependsExternal('Tests\Features\Depends', 'second');
+
+test('dependsExternal chained, reversed', function () {
+    expect(func_get_args())->toBe(['second', 'first']);
+})->dependsExternal('Tests\Features\Depends', 'second')
+    ->dependsExternal('Tests\Features\Depends', 'first');
+
 test('depends on tests in an external file', function (string $first, string $second) {
     expect($first)->toBe('first');
     expect($second)->toBe('second');
