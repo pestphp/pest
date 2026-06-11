774a34040029078a0a2bab1828b4d9104dec6aec
Sonali dudhia <45190968+sonalidudhia@users.noreply.github.com>
2026-06-11 14:44:00 +0530

    feat: add toBeUlid assertion and isUlid validation method (#1726)

--- src/Mixins/Expectation.php
+++ src/Mixins/Expectation.php
@@ -1143,6 +1143,22 @@ final class Expectation
     }
 
     /**
+     * Asserts that the value is a ULID.
+     *
+     * @return self<TValue>
+     */
+    public function toBeUlid(string $message = ''): self
+    {
+        if (! is_string($this->value)) {
+            InvalidExpectationValue::expected('string');
+        }
+
+        Assert::assertTrue(Str::isUlid($this->value), $message);
+
+        return $this;
+    }
+
+    /**
      * Asserts that the value is between 2 specified values
      *
      * @return self<TValue>
